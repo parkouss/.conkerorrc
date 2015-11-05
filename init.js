409fc79189e125d45c2572def3de180401f6f551
Julien Pagès <j.parkouss@gmail.com>
2015-11-05 16:46:46 +0100

    initial commit

---
+++ init.js
@@ -0,0 +1,17 @@
+// use sessions
+require("session.js");
+session_auto_save_auto_load = true;
+session_save_buffer_access_order = true;
+
+
+// load urls from the command line in new buffers instead
+// of new windows.
+url_remoting_fn = load_url_in_new_buffer;
+
+
+// load download buffers in the background in the current
+// window, instead of in new windows.
+download_buffer_automatic_open_target = OPEN_NEW_BUFFER_BACKGROUND;
+
+require("favicon");
+read_buffer_show_icons = true;
