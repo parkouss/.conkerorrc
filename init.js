c726b85300b39e53d77643a0a45fad27eff3b446
Julien Pagès <j.parkouss@gmail.com>
2016-05-01 11:02:21 +0200

    enable duckduckgo page mode

--- init.js
+++ init.js
@@ -36,6 +36,7 @@ add_hook("mode_line_hook", mode_line_adder(buffer_count_widget), true);
 require('google-search-results');
 require('google-images');
 require('google-maps');
+require('duckduckgo');
 require('hackernews');
 
 
